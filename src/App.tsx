c839aa6e268f76b21c0e8d8aa70d7ea3d07c5ca4
lee <lee@dev.com>
2022-09-07 07:50:32 -0700

    for heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { RootRouter } from "./navigation";
 
 function App() {
--- src/navigation/RootRouter.tsx
+++ src/navigation/RootRouter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomeScreen } from "../features/home";
 import { QuizScreen } from "../features/quiz";
